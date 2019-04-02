7dd6cd55048c10e96f779a2d549ec8d4e5e37827
lemon4ex <netchen@vip.qq.com>
2019-04-02 23:21:43 +0800

    Initial Commit

---
+++ CSUnlocker/CSUnlocker.h
@@ -0,0 +1,9 @@
+//
+//  CSUnlocker.h
+//  CSUnlocker
+//
+//  Created by h4ck on 2019/4/2.
+//  Copyright (c) 2019年 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ CSUnlocker/CSUnlocker.m
@@ -0,0 +1,15 @@
+//
+//  CSUnlocker.m
+//  CSUnlocker
+//
+//  Created by h4ck on 2019/4/2.
+//  Copyright (c) 2019年 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import "CSUnlocker.h"
+#import <CaptainHook/CaptainHook.h>
+
+CHConstructor // code block that runs immediately upon load
+{
+    NSLog(@"++++++++ CSUnlocker loaded ++++++++");
+}
